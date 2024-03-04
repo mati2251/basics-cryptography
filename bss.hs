03c891a990840fe8f63bf9c27b4f40df4708708e
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-03-04 17:43:21 +0100

    Added bss generator

--- bss.hs
+++ bss.hs
@@ -1,23 +1,47 @@
-minus :: (Ord a) => [a] -> [a] -> [a]
-minus [] _ = []
-minus xs [] = xs
-minus (x : xs) (y : ys) = case compare x y of
-  LT -> x : minus xs (y : ys)
-  EQ -> minus xs ys
-  GT -> minus (x : xs) ys
+{-# LANGUAGE NumDecimals #-}
+import Math.NumberTheory.Primes
+import Foreign (newArray0, Bits (xor))
+import Control.Monad (replicateM)
+import Data.Foldable (foldl')
 
-primesTo m = 2 : sieve [3, 5 .. m]
-  where
-    sieve (p : xs)
-      | p * p > m = p : xs
-      | otherwise = p : sieve (xs `minus` [p * p, p * p + 2 * p ..])
+bigInt :: Integer
+bigInt = 10e9
 
-from = 10 ^ 10 - 10 ^ 5
+p = until (\x -> x `mod` 4 == 3) (unPrime . nextPrime) bigInt
+q = until (\x -> x `mod` 4 == 3) (unPrime . nextPrime) p
+n = p * q
 
-to = 10 ^ 7
+nwd :: Integer -> Integer -> Integer
+nwd a 0 = a
+nwd a b = nwd b (a `mod` b)
 
-p = filter (>= from) $ primesTo to
+lsb :: Integer -> Integer
+lsb x = x `mod` 2
+
+randomCoprime :: Integer -> Integer
+randomCoprime 0 = 1 
+randomCoprime n = until (\x -> nwd x n == 1) (\x -> x - 1)  n
+
+randomCoprimes :: Int -> Integer -> [Integer]
+randomCoprimes x n = take x (iterate randomCoprime n)
+
+randomBits :: [Integer] -> [Integer]
+randomBits = map (\ x -> lsb (x ^ 2) `mod` n)
+
+bitsToNumber :: [Integer] -> Integer 
+bitsToNumber = foldl' (\acc x -> acc * 2 + x) 0
+
+countOnes :: [Integer] -> Int
+countOnes = foldl' (\acc x -> if x == 1 then acc + 1 else acc) 0
+
+bits = randomBits (randomCoprimes 20000 n)
+bitsCount = countOnes bits 
+
+number = bitsToNumber bits
 
 main :: IO ()
-main = print p
-
+main = do
+  print "Random number"
+  print number
+  print "Number of ones"
+  print bitsCount
